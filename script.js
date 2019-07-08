244208488a8a35e2c0bffa7730955ec093aa807d
Anton <khebnev.a.v@gmail.com>
2019-07-08 23:03:27 +0300

    Added a part of about

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-menu.onclick = function myFunction(){
+menu.onclick = function myFunction() {
     var x = document.getElementById('myTopnav');
-    if (x.className === "topnav"){ //Если переменная Х.className будет идентична классу topnav тогда к нашему классу будет добавляться слово responsive
-        x.className += "responsive";
-        }
+
+    if (x.className === "topnav") { //Если переменная Х.className будет идентична классу topnav тогда к нашему классу будет добавляться слово responsive
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
